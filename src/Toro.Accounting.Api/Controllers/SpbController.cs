e5918c524939aaba7c761d6d857226aebef9ef36
Thiago Inhan <thiagoinhan@gmail.com>
2022-06-06 00:46:09 -0300

    Adiciona estrutura inicial da api
    Adiciona o controller que será responsável por expor o serviço de
    depósito.
    
    Feature: TORO-003

---
+++ src/Toro.Accounting.Api/Controllers/SpbController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Toro.Accounting.Api.Dtos;
+using Toro.Accounting.Application.Commands;
+using Toro.Accounting.Application.Contracts;
+
+namespace Toro.Accounting.Controllers;
+
+[ApiController]
+[Route("spb")]
+public class SpbController : ControllerBase
+{
+    protected readonly ICommandDispatcher _commandDispatcher;
+
+    public SpbController(ICommandDispatcher commandDispatcher)
+    {
+        _commandDispatcher = commandDispatcher;
+    }
+
+    [Route("events")]
+    [HttpPost()]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(MakeDepositCommandResponse), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> MakeDeposit(DepositEventDetails depositDetails)
+    {
+        var command = new MakeDepositCommand(depositDetails.target.account, depositDetails.origin.cpf, depositDetails.amount);
+        var response = await _commandDispatcher.Dispatch(command);
+
+        if (response.ValidationErrors.Any())
+        {
+            return BadRequest(response.ValidationErrors);
+        }
+
+        return NoContent();
+    }
+}
---
+++ src/Toro.Accounting.Api/Dtos/DepositEventDetails.cs
@@ -0,0 +1,6 @@
+﻿namespace Toro.Accounting.Api.Dtos
+{
+    public record DepositEventDetails(string @event, TargetAccountDetails target, OriginAccountDetails origin, decimal amount);
+    public record TargetAccountDetails(string bank, string branch, string account);
+    public record OriginAccountDetails(string bank, string branch, string cpf);
+}
