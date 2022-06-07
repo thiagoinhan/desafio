265fb2100aa6677dacfd4e5f93aaf42b969b51db
Thiago Inhan <thiagoinhan@gmail.com>
2022-06-06 23:07:25 -0300

    Organiza projetos de teste e cria o primeiro mock
    Organiza os nomes dos projetos para ficar mais intuitivo
    
    Adicionalmente cria o mock inicial do CustomerRepository e centraliza no
    projeto Toro.Accounting.Commom.UnitTests, que será utilizado pelos
    outros projetos de testes
    
    Feature: TORO-003

---
+++ test/Toro.Accounting.Commom.UnitTests/Mocks/RepositoryMocks.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using Toro.Accounting.Application.Contracts.Persistence;
+using Toro.Accounting.Domain.Entities;
+
+namespace Toro.Accounting.Commom.UnitTests.Mocks
+{
+    public static class RepositoryMocks
+    {
+        public static Mock<ICustomerRepository> GetCustomerRepository()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer(Guid.NewGuid(), "luffy", "Monkey D. Luffy", "87498578964", "283749", 2000),
+                new Customer(Guid.NewGuid(), "tanjiro", "Tanjiro Kamado", "74894803977", "839456", 0),
+                new Customer(Guid.NewGuid(), "eren", "Eren Yeager", "17384975822", "139029", 1000),
+            };
+
+            var mockCustomerRepository = new Mock<ICustomerRepository>();
+            mockCustomerRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(customers);
+
+            mockCustomerRepository.Setup(repo => repo.CreateAsync(It.IsAny<Customer>())).Returns(
+                (Customer customer) =>
+                {
+                    customers.Add(customer);
+                    return customer;
+                });
+
+            return mockCustomerRepository;
+        }
+    }
+}
